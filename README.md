6dd00a6281d6b09640c871999b62842bdd594af8
Steve Lau <stevelauc@outlook.com>
2023-06-05 20:22:58 +0800

    docs: README & test: for insertion

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 ## BPlusTreeSet
 
 A set implementation backed by B+Tree.
+
+## API
+
+```rust
+pub fn new(order: usize) -> Self
+pub fn len(&self) -> usize
+pub fn insert(&mut self, value: T) -> bool
+pub fn remove<Q>(&mut self, value: &Q) -> bool
+pub fn take<Q>(&mut self, value: &Q) -> Option<T>
+pub fn contains<Q>(&self, value: &Q) -> bool
+```
+
+## Basic Usage
+
+```rust
+# use b_plus_tree_set::BPlusTreeSet;
+let mut set = BPlusTreeSet::new(4);
+
+for i in 0..10 {
+    assert!(!set.contains(&i));
+    assert!(set.insert(i));
+}
+
+assert_eq!(set.len(), 10);
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,36 @@
+#![doc = include_str!("../README.md")]
+
 mod node;
 mod tree;
 mod util;
 
 pub use tree::BPlusTreeSet;
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn new() {
+        let set: BPlusTreeSet<i32> = BPlusTreeSet::new(4);
+
+        assert_eq!(set.len(), 0);
+    }
+
+    #[test]
+    fn insert() {
+        let mut set = BPlusTreeSet::new(4);
+
+        for i in 0..10 {
+            assert!(!set.contains(&i));
+            assert!(set.insert(i));
+        }
+
+        for i in 0..10 {
+            assert!(!set.insert(i));
+            assert!(set.contains(&i));
+        }
+
+        assert_eq!(set.len(), 10);
+    }
+}
